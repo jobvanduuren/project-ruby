ef9eaead0c231c607ec82e7ab8144c6dcd2f4d2e
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-27 22:14:03 +0200

    create seeds for student

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+student1 = Student.create!( { name: "Paul McCann", image_url: "https://content-static.upwork.com/uploads/2014/10/02123010/profile-photo_friendly.jpg" } )
+student2 = Student.create!( { name: "Robin Smith", image_url: "http://www.st2299.com/data/wallpapers/65/wp-image-58225497.jpg" } )
+student3 = Student.create!( { name: "Mary Tanaka", image_url: "https://upload.wikimedia.org/wikipedia/commons/6/65/Kruse_CNDLS_Profile.png" } )
+student4 = Student.create!( { name: "Steve Sawyer", image_url: "http://ratzinger.cl/wp-content/uploads/2016/06/profile-picture-15.jpg"} )
