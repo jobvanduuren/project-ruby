f08604188d04bd11a99f4bfbe17e68c7b6812d14
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-28 14:29:35 +0200

    add validations to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  validates :first_name, presence: true
+  validates :surname, presence: true
+
+  validates :image_url, length: { maximum: 400 }, allow_blank: true
+
 end
