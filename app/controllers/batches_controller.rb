6644099aa756feb5b68ca98dddfb4a8b2890a7d9
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-28 15:30:53 +0200

    add batch model controller index view and routes

---
+++ app/controllers/batches_controller.rb
@@ -0,0 +1,6 @@
+class BatchesController < ApplicationController
+
+  def index
+    @batches = Batch.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "students#index"
 
   resources :students
+  resources :batches
 
 end
