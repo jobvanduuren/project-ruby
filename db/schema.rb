4b61fa6bb96dab85d1d45c100d3f993edd9d1624
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-28 14:55:48 +0200

    create model Batch

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628095729) do
+ActiveRecord::Schema.define(version: 20170628125203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "batches", force: :cascade do |t|
+    t.integer "batch_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "image_url"
