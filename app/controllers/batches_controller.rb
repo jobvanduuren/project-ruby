cf1347b75842ffe2eeee6b57795052ab9100b1a7
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-28 18:08:14 +0200

    add edit for batch in view and controller, add new batch link in batch index

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -13,5 +13,36 @@ class BatchesController < ApplicationController
     @batch = Batch.new
   end
 
+  def create
+    @batch = Batch.new(batch_params)
+
+    if @batch.save
+      redirect_to @batch
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @batch = Batch.find(params[:id])
+  end
+
+  def update
+    @batch = Batch.find(params[:id])
+
+    if @batch.update_attributes(batch_params)
+      redirect_to @batch
+    else
+      render 'edit'
+    end
+  end
+
+
+private
+
+  def batch_params
+    params.require(:batch).permit(:batch_number, :start_date, :end_date)
+  end
+
 
 end
