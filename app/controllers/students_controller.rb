4eb2e7a699d69d897cefb719238745d5daacdeb8
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-28 12:59:56 +0200

    create a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,4 +12,16 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    student_params = params.require(:student).permit(:first_name, :surname, :image_url)
+
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
 end
