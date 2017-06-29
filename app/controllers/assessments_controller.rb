ce1e5999ce49cf7221f5d9824ec92c0bafd61f5b
jobvanduuren <job_van_duuren@hotmail.com>
2017-06-29 13:52:24 +0200

    add colours to student show page

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,2 +1,45 @@
 class AssessmentsController < ApplicationController
+
+  def new
+    @assessment = Assessment.new
+  end
+
+  def create
+    @assessment = Assessment.new(assessment_params)
+
+    if @assessment.save
+      redirect_to batches_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @assessment = Assessment.find(params[:id])
+  end
+
+  def update
+    @assessment = Assessment.find(params[:id])
+
+    if @assessment.update_attributes(assessment_params)
+      redirect_to batches_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @assessment = Assessment.find(params[:id])
+
+    @assessment.destroy
+
+    redirect_to batches_path
+  end
+
+private
+
+  def assessment_params
+    params.require(:assessment).permit(:colour, :comment, :date)
+  end
+
 end
