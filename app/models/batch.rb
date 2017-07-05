a3645905ae0f88b8bce786ff50aa26078c218a1e
jobvanduuren <job_van_duuren@hotmail.com>
2017-07-05 16:44:53 +0200

    add buttons to student show and add dependency destroy to batch and student models

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,9 +1,11 @@
 class Batch < ApplicationRecord
 
+  has_many :students, dependent: :destroy
+
   validates :batch_number, presence: true
   validates :batch_number, uniqueness: true
   validates :start_date, presence: true
   validates :end_date, presence: true
 
-  has_many :students
+
 end
